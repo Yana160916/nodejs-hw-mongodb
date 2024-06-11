bef2344a24b013aac5c5457fb29796ef3d6b8dce
Yana160916 <144559656+Yana160916@users.noreply.github.com>
2024-06-11 15:05:29 +0300

    update

--- src/server.js
+++ src/server.js
@@ -40,25 +40,25 @@ export const setupServer = () => {
     }
   });
 
-  app.get('/students/:studentId', async (req, res) => {
-    const { studentId } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(studentId)) {
+  app.get('/students/:studentsId', async (req, res) => {
+    const { students_Id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(students_Id)) {
       return res.status(404).json({
         status: 404,
-        message: `Student with id ${studentId} not found`,
+        message: `Student with id ${students_Id} not found`,
       });
     }
-    const student = await getStudentById(studentId);
+    const student = await getStudentById(students_Id);
 
     if (!student) {
       return res.status(404).json({
         status: 404,
-        message: `Student with id ${studentId} not found`,
+        message: `Student with id ${students_Id} not found`,
       });
     }
     res.status(200).json({
       status: 200,
-      message: `Successfully found student with id ${studentId}!`,
+      message: `Successfully found student with id ${students_Id}!`,
       data: student,
     });
   });
