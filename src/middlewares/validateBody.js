40bf6585f1aaa9a5dc166b4d4c3b027936ed138d
Yana160916 <144559656+Yana160916@users.noreply.github.com>
2024-06-25 11:30:28 +0300

    update

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,15 +1,15 @@
-import createHttpError from 'http-errors';
+import { createContactSchema } from '../validation/contacts.js';
 
-export const validateBody = (schema) => async (req, res, next) => {
-  try {
-    await schema.validateAsync(req.body, {
-      abortEarly: false,
+export const validateCreateContact = (req, res, next) => {
+  const { error } = createContactSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (error) {
+    return res.status(400).json({
+      status: 400,
+      message: 'Validation Error',
+      details: error.details.map((detail) => detail.message),
     });
-    next();
-  } catch (err) {
-    const error = createHttpError(400, 'Bad Request', {
-      errors: err.details,
-    });
-    next(error);
   }
+  next();
 };
