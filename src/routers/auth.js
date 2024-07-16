6a6bf42f0735ec8a2b0f72a8d5c0ead0aeb2001f
Yana160916 <144559656+Yana160916@users.noreply.github.com>
2024-07-16 12:20:14 +0200

    update

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -15,6 +15,9 @@ import {
   requestResetEmailController,
   resetPasswordController,
 } from '../controllers/auth.js';
+import { getGoogleOAuthUrlController } from '../controllers/auth.js';
+import { loginWithGoogleOAuthSchema } from '../validation/auth.js';
+import { loginWithGoogleController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -44,6 +47,14 @@ router.post(
   '/reset-password',
   validateBody(resetPasswordSchema),
   ctrlWrapper(resetPasswordController),
+);
+
+router.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+router.post(
+  '/confirm-oauth',
+  validateBody(loginWithGoogleOAuthSchema),
+  ctrlWrapper(loginWithGoogleController),
 );
 
 export default router;
